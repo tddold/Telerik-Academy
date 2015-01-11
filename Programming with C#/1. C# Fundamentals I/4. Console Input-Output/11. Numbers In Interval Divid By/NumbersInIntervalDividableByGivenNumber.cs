d029427244be76bf166e5fe74ea200c960f2f65a
Todor Dakov <tdd@dekom.bg>
2015-01-11 12:54:24 +0200

    Update

---
+++ Programming with C#/1. C# Fundamentals I/4. Console Input-Output/11. Numbers In Interval Divid By/NumbersInIntervalDividableByGivenNumber.cs
@@ -0,0 +1,32 @@
+﻿/*Problem 11.* Numbers in Interval Dividable by Given Number
+Write a program that reads two positive integer numbers and prints how many numbers p exist between them such that the reminder of the division by 5 is 0.*/
+
+using System;
+
+class NumbersInIntervalDividableByGivenNumber
+{
+    static void Main()
+    {
+        Console.Title = "Numbers in Interval Dividable by Given Number";
+
+        Console.WriteLine("Numbers in Interval Dividable by Given Number!");
+        Console.WriteLine(new string('-', 40));
+        Console.Write("Enter positive integer numbers a: ");
+        int start = int.Parse(Console.ReadLine());
+        Console.Write("Enter positive integer numbers b: ");
+        int end = int.Parse(Console.ReadLine());
+
+        int result = 0;
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 5 == 0)
+            {
+                result++;
+            }
+        }
+
+        Console.WriteLine(new string('-', 40));
+        Console.WriteLine("Result is: {0}", result);
+        Console.WriteLine(new string('-', 40));
+    }
+}
