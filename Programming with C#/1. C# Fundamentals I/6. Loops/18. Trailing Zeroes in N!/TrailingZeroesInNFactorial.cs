b951f69e6a57382e4868dfd1ecbb5e9667fc917a
Todor Dakov <tdd@dekom.bg>
2015-01-14 00:29:33 +0200

    Create Problem 18.* Trailing Zeroes in N! and Problem 19.** Spiral Matrix

---
+++ Programming with C#/1. C# Fundamentals I/6. Loops/18. Trailing Zeroes in N!/TrailingZeroesInNFactorial.cs
@@ -0,0 +1,65 @@
+﻿/*Problem 18.* Trailing Zeroes in N!
+Write a program that calculates with how many zeroes the factorial of a given number n has at its end.
+Your program should work well for very big numbers, e.g. n=100000.*/
+
+using System;
+using System.Numerics;
+
+class TrailingZeroesInNFactorial
+{
+    static void Main()
+    {
+        Console.Title = "Trailing Zeroes in N!";
+
+        // # First method - it is very slow
+        // --------------------------------
+
+        // input
+        Console.WriteLine("Enter number");
+        Console.WriteLine(new string('-', 40));        
+        Console.Write("n  --> ");
+        int n = int.Parse(Console.ReadLine());
+
+        BigInteger nFactorel = 1;
+        nFactorel = GetNFactorial(n, nFactorel);
+
+        Console.WriteLine("n! --> {0}", nFactorel);
+        Console.WriteLine(new string('_', 80));
+
+        // logic        
+        int countZero = 0;       
+        while (nFactorel % 10 == 0)
+        {
+            nFactorel = nFactorel / 10;
+            countZero++;
+        }
+        // output
+        Console.WriteLine("First method: {0}", countZero);
+
+        // # Second method - it is very fast
+        // ---------------------------------
+        int factorFive = 5;
+        int dividParameter = 0;
+        int count = 1;
+        countZero = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            dividParameter = (int) Math.Pow(factorFive, count);
+            countZero += n / dividParameter;
+            count++;
+        }
+
+        Console.WriteLine("Second method: {0}", countZero);
+    }
+
+    private static BigInteger GetNFactorial(int n, BigInteger nFactorel)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            nFactorel *= i;
+        }
+
+        return nFactorel;
+    }
+}
---
+++ Programming with C#/1. C# Fundamentals I/6. Loops/19. Spiral Matrix/SpiralMatrix.cs
@@ -0,0 +1,79 @@
+﻿/*Problem 19.** Spiral Matrix
+Write a program that reads from the console a positive integer number n (1 = n = 20) and prints a matrix holding the numbers from 1 to n*n in the form of square spiral like in the examples below.*/
+
+using System;
+
+class SpiralMatrix
+{
+    static void Main()
+    {
+        Console.Title = "Spiral Matrix";
+
+        Console.Write("Enter number between [1-20]: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(new string('-', 40));
+
+        int[,] matrix = new int[n, n];
+        string direction = "right";
+        int row = 0;
+        int col = 0;
+
+        for (int i = 1; i <= n * n; i++)
+        {
+            if (direction == "right" && (col >= n || matrix[row, col] != 0))
+            {
+                col--;
+                row++;
+                direction = "down";
+            }
+            else if (direction == "down" && (row >= n || matrix[row, col] != 0))
+            {
+                row--;
+                col--;
+                direction = "left";
+            }
+            else if (direction == "left" && (col < 0 || matrix[row, col] != 0))
+            {
+                col++;
+                row--;
+                direction = "up";
+            }
+            else if (direction == "up" && (row < 0 || matrix[row, col] != 0))
+            {
+                row++;
+                col++;
+                direction = "right";
+            }
+
+            matrix[row, col] = i;
+
+            if (direction == "right")
+            {
+                col++;
+            }
+            else if (direction == "down")
+            {
+                row++;
+            }
+            else if (direction == "left")
+            {
+                col--;
+            }
+            else if (direction == "up")
+            {
+                row--;
+            }
+        }
+
+        // Print matrix
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write("{0,4}", matrix[i, j]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
