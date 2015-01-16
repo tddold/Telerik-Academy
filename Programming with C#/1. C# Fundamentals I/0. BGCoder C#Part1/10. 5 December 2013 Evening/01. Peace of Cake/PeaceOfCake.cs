be20fbbcd7fc49b0ebdd77467211c849f7acdd33
Todor Dakov <tdd@dekom.bg>
2015-01-16 02:30:16 +0200

    Create PeaceOfCake and AngryFemaleGPS

---
+++ Programming with C#/1. C# Fundamentals I/0. BGCoder C#Part1/10. 5 December 2013 Evening/01. Peace of Cake/PeaceOfCake.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Peace_of_Cake
+{
+    class PeaceOfCake
+    {
+        static void Main()
+        {
+            long a = long.Parse(Console.ReadLine());
+            long b = long.Parse(Console.ReadLine());
+            long c = long.Parse(Console.ReadLine());
+            long d = long.Parse(Console.ReadLine());
+
+            long nominator = (a * d) + (b * c);
+            long denominator = b * d;
+
+            if (nominator >= denominator)
+            {
+                long resultInteger = nominator / denominator;
+
+                Console.WriteLine(resultInteger);
+                Console.WriteLine("{0}/{1}", nominator, denominator);
+            }
+            else
+            {
+                decimal resultDouble = nominator / (decimal) denominator;
+
+                Console.WriteLine("{0:f22}", resultDouble);
+                Console.WriteLine("{0}/{1}", nominator, denominator);
+            }
+        }
+    }
+}
---
+++ Programming with C#/1. C# Fundamentals I/0. BGCoder C#Part1/10. 5 December 2013 Evening/02. Angry Female GPS/AngryFemaleGPS.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Angry_Female_GPS
+{
+    class AngryFemaleGPS
+    {
+        static void Main()
+        {
+            // input
+            string n = Console.ReadLine();
+
+            // solution
+            long sumEven = 0;
+            long sumOdd = 0;
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (n[i] == '-')
+                {
+                    continue;
+                }
+                if (((long) n[i] -48) % 2 == 0)
+                {
+                    sumEven += (long) n[i] - 48;
+                }
+                else
+                {
+                    sumOdd += (long) n[i]-48;
+                }
+            }
+
+            // print result
+            if (sumOdd == sumEven)
+            {
+                Console.WriteLine("straight {0}", sumOdd);
+            }
+            else if (sumOdd > sumEven)
+            {
+                Console.WriteLine("left {0}", sumOdd);
+            }
+            else
+            {
+                Console.WriteLine("right {0}", sumEven);
+            }
+        }
+    }
+}
