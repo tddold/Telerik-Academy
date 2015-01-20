be6a7b7b52ea540242805d364fbb205c7c740b6f
Todor Dakov <tdd@dekom.bg>
2015-01-20 02:42:57 +0200

    Create Garden and TheSecretOfNumbers

---
+++ Programming with C#/1. C# Fundamentals I/0. BGCoder C#Part1/07. 24 June 2013 Evening/01. Garden/Garden.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Garden
+
+{
+    static void Main()
+    {
+        const double tomatPrice = 0.5;
+        const double cucumberPrice = 0.4;
+        const double potatoPrice = 0.25;
+        const double carrotPrice = 0.6;
+        const double cabbagePrice = 0.3;
+        const double beansPrice = 0.4;
+        const double totalArea = 250;
+
+
+        double tomatoSeed = double.Parse(Console.ReadLine());
+        double tomatoArea = double.Parse(Console.ReadLine());
+        double cucumberSeed = double.Parse(Console.ReadLine());
+        double cucumberArea = double.Parse(Console.ReadLine());
+        double potatoSeed = double.Parse(Console.ReadLine());
+        double potatoArea = double.Parse(Console.ReadLine());
+        double carrotSeed = double.Parse(Console.ReadLine());
+        double carrotArea = double.Parse(Console.ReadLine());
+        double cabbageSeed = double.Parse(Console.ReadLine());
+        double cabbageArea = double.Parse(Console.ReadLine());
+        double beansSeed = double.Parse(Console.ReadLine());
+
+        //Console.WriteLine(tomatoSeed + "-" + tomatoArea);
+        //Console.WriteLine(cucumberSeed + "-" + cucumberArea);
+        //Console.WriteLine(potatoSeed + "-" + potatoArea);
+
+        double tomatoCost = tomatoSeed * tomatPrice;
+        double cucmberCost = cucumberSeed * cucumberPrice;
+        double potatoCost = potatoSeed * potatoPrice;
+        double carrotCost = carrotSeed * carrotPrice;
+        double cabbageCost = cabbageSeed * cabbagePrice;
+        double beansCost = beansSeed * beansPrice;
+
+        double totalCost = tomatoCost + cucmberCost + potatoCost + carrotCost + cabbageCost + beansCost;
+        Console.WriteLine("Total costs:{0: 0.00}", totalCost);
+
+        double currentArea = tomatoArea + cucumberArea + potatoArea + carrotArea + cabbageArea;
+
+        if (currentArea > totalArea)
+        {
+            Console.WriteLine("Insufficient area");
+        }
+        else if (currentArea == totalArea)
+        {
+            Console.WriteLine("No area for beans");
+        }
+        else
+        {
+            Console.WriteLine("Beans area:{0: 0}", totalArea- currentArea);
+        }
+    }
+}
---
+++ Programming with C#/1. C# Fundamentals I/0. BGCoder C#Part1/07. 24 June 2013 Evening/02.TheSecretOfNumbers/TheSecretOfNumbers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class TheSecretOfNumbers
+{
+    static void Main()
+    {
+        string n = Console.ReadLine();
+        char[] removeSymbols = new char[] { '-' };
+        n = n.TrimStart(removeSymbols);
+       
+        long secretSum = 0;
+        long eventSecretSum = 0;
+        long oddSecretSum = 0;
+
+        for (int i = 0; i < n.Length; i++)
+        {
+            if ((i + 1) % 2 == 0)
+            {
+                eventSecretSum += ((long.Parse(n[(n.Length - 1) - i].ToString()) * long.Parse(n[(n.Length - 1) - i].ToString())) * (i + 1));
+            }
+            else
+            {
+                oddSecretSum += (long.Parse(n[(n.Length - 1) - i].ToString()) * ((i + 1) * (i + 1)));
+            }
+        }
+
+        secretSum = eventSecretSum + oddSecretSum;
+
+        long checklastDigits = secretSum % 10;
+        long remainder = 0;
+        string secretAlphaSequence = String.Empty;
+        if (checklastDigits == 0)
+        {
+            Console.WriteLine(secretSum);
+            Console.WriteLine("{0} has no secret alpha-sequence", n);
+        }
+        else
+        {
+            remainder = secretSum % 26;
+            string alphaBet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            for (int i = 0; i < checklastDigits; i++)
+            {
+                while (remainder > 25)
+                {
+                    remainder -= 26;
+                }
+
+                int index = (int) remainder;
+                secretAlphaSequence += alphaBet[index];
+                remainder++;
+            }
+
+            Console.WriteLine(secretSum);
+            Console.WriteLine(secretAlphaSequence);
+        }
+    }
+}
