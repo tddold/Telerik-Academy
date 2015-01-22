a6634ca1b9abb19362e68bb80dab0cf34aee80b3
Todor Dakov <tdd@dekom.bg>
2015-01-22 02:47:05 +0200

    Update

--- Programming with C#/1. C# Fundamentals I/0. BGCoder C#Part1/07. 24 June 2013 Evening/03. Card Wars/CardWars.cs
+++ Programming with C#/1. C# Fundamentals I/0. BGCoder C#Part1/07. 24 June 2013 Evening/03. Card Wars/CardWars.cs
@@ -8,132 +8,119 @@ class CardWars
     {
         // http://bgcoder.com/Contests/Practice/Index/93#2
 
-        //if (Environment.CurrentDirectory.ToLower().EndsWith("bin\\debug"))
-        //{
-        //    Console.SetIn(new StreamReader("input.txt"));
-        //}
+        if (Environment.CurrentDirectory.ToLower().EndsWith("bin\\debug"))
+        {
+            Console.SetIn(new StreamReader("input.txt"));
+        }
+
 
         int numberOfGames = int.Parse(Console.ReadLine());
 
-        int scoreFirstPlayer = 0;
-        int scoreSecondPlayer = 0;
+        const int numberCards = 3;
+
         int winsFirstPlayer = 0;
         int winsSecondPlaer = 0;
-        int winMatchFirstPlayer = 0;
-        int winMatchSecondPlayer = 0;
+       // int winMatchFirstPlayer = 0;
+       // int winMatchSecondPlayer = 0;
+
         BigInteger totalScoreFirstPlayer = 0;
         BigInteger totalScoreSecondPlayer = 0;
+
+        bool isXCardFirstPlayer = false;
+        bool isXCardSecondPlayer = false;
 
         // input
         for (int i = 0; i < numberOfGames; i++)
         {
-            string cardsFirstPlayer = Console.ReadLine() +
-                                      Console.ReadLine() +
-                                      Console.ReadLine();
-
-            int indexOfZero = 0;
-
-            cardsFirstPlayer = RemoveZeroFromString(cardsFirstPlayer, indexOfZero);
-
-
-            string cardsSecondPlayer = Console.ReadLine() +
-                                       Console.ReadLine() +
-                                       Console.ReadLine();
-
-            cardsSecondPlayer = RemoveZeroFromString(cardsSecondPlayer, indexOfZero);
-
-            // null score for next game
-            scoreFirstPlayer = 0;
-            scoreSecondPlayer = 0;
-            winMatchFirstPlayer = 0;
-            winMatchSecondPlayer = 0;
-
             // score for first player
-            for (int j = 0; j < cardsFirstPlayer.Length; j++)
+            int scoreFirstPlayer = 0;
+            for (int j = 0; j < numberCards; j++)
             {
-                if (cardsFirstPlayer[j] == 'X')
-                {
-                    winMatchFirstPlayer++;
-                    break;
-                }
-                else
-                {
-                    if (cardsFirstPlayer[j] == 'Z' || cardsFirstPlayer[j] == 'Y')
-                    {
-                        totalScoreFirstPlayer = CheckForSpecialCards(cardsFirstPlayer,
-                                                             j, totalScoreFirstPlayer);
-                    }
-                    else
-                    {
-                        scoreFirstPlayer = GetScoreForGame(scoreFirstPlayer, cardsFirstPlayer[j].ToString());
-                    }
+                string cardsFirstPlayer = Console.ReadLine();
 
+                switch (cardsFirstPlayer)
+                {
+                    case "A":
+                        scoreFirstPlayer += 1;
+                        break;
+                    case "J":
+                        scoreFirstPlayer += 11;
+                        break;
+                    case "Q":
+                        scoreFirstPlayer += 12;
+                        break;
+                    case "K":
+                        scoreFirstPlayer += 13;
+                        break;
+                    case "Z":
+                        totalScoreFirstPlayer *= 2;
+                        break;
+                    case "Y":
+                        totalScoreFirstPlayer -= 200;
+                        break;
+                    case "X":
+                        isXCardFirstPlayer = true;
+                        break;
+                    default:
+                        scoreFirstPlayer += 12 - int.Parse(cardsFirstPlayer);
+                        break;
                 }
             }
 
-            // score for second player
-            for (int j = 0; j < cardsSecondPlayer.Length; j++)
+            // score for second player  
+            int scoreSecondPlayer = 0;
+            for (int j = 0; j < numberCards; j++)
             {
-                if (cardsSecondPlayer[j] == 'X')
+                string cardsSecondPlayer = Console.ReadLine();
+
+                switch (cardsSecondPlayer)
                 {
-                    winMatchSecondPlayer++;
-                    break;
-                }
-                else
-                {
-                    if (cardsSecondPlayer[j] == 'Z' || cardsSecondPlayer[j] == 'Y')
-                    {
-                        totalScoreSecondPlayer = CheckForSpecialCards(cardsSecondPlayer,
-                                                             j, totalScoreSecondPlayer);
-                        if (i == numberOfGames - 1 && j == cardsSecondPlayer.Length - 1)
-                        {
-                            if (totalScoreFirstPlayer > totalScoreSecondPlayer)
-                            {
-                                Console.WriteLine("First player wins!");
-                                Console.WriteLine("Score: {0}", totalScoreFirstPlayer);
-                                Console.WriteLine("Games won: {0}", winsFirstPlayer);
-                                return;
-                            }
-                            else if (totalScoreFirstPlayer < totalScoreSecondPlayer)
-                            {
-                                Console.WriteLine("Second player wins!");
-                                Console.WriteLine("Score: {0}", totalScoreSecondPlayer);
-                                Console.WriteLine("Games won: {0}", winsSecondPlaer);
-                                return;
-                            }
-                            else if (totalScoreFirstPlayer == totalScoreSecondPlayer)
-                            {
-                                Console.WriteLine("It's a tie!");
-                                Console.WriteLine("Score: {0}", totalScoreSecondPlayer);
-                                return;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        scoreSecondPlayer = GetScoreForGame(scoreSecondPlayer, cardsSecondPlayer[j].ToString());
-                    }
+                    case "A":
+                        scoreSecondPlayer += 1;
+                        break;
+                    case "J":
+                        scoreSecondPlayer += 11;
+                        break;
+                    case "Q":
+                        scoreSecondPlayer += 12;
+                        break;
+                    case "K":
+                        scoreSecondPlayer += 13;
+                        break;
+                    case "Z":
+                        totalScoreSecondPlayer *= 2;
+                        break;
+                    case "Y":
+                        totalScoreSecondPlayer -= 200;
+                        break;
+                    case "X":
+                        isXCardSecondPlayer = true;
+                        break;
+                    default:
+                        scoreSecondPlayer += 12 - int.Parse(cardsSecondPlayer);
+                        break;
                 }
             }
 
             // check wins
-
-            if (winMatchFirstPlayer > 0 && winMatchSecondPlayer > 0)
+            if (isXCardFirstPlayer && isXCardSecondPlayer)
             {
                 totalScoreFirstPlayer += 50;
                 totalScoreSecondPlayer += 50;
+
+                isXCardFirstPlayer = false;
+                isXCardSecondPlayer = false;
             }
-            else if (winMatchFirstPlayer > 0)
+            else if (isXCardFirstPlayer)
             {
-                Console.WriteLine("X card drawn! Player one wins the match!");
-                return;
+                break;
             }
-            else if (winMatchSecondPlayer > 0)
+            else if (isXCardSecondPlayer)
             {
-                Console.WriteLine("X card drawn! Player two wins the match!");
-                return;
+                break;
             }
-            else if (scoreFirstPlayer > scoreSecondPlayer)
+            
+            if (scoreFirstPlayer > scoreSecondPlayer)
             {
                 winsFirstPlayer++;
                 totalScoreFirstPlayer += scoreFirstPlayer;
@@ -145,13 +132,21 @@ class CardWars
             }
         }
 
-        if (winsFirstPlayer > winsSecondPlaer)
+        if (isXCardFirstPlayer)
+        {
+            Console.WriteLine("X card drawn! Player one wins the match!");
+        }
+        else if (isXCardSecondPlayer)
+        {
+            Console.WriteLine("X card drawn! Player two wins the match!");
+        }
+        else if (totalScoreFirstPlayer > totalScoreSecondPlayer)
         {
             Console.WriteLine("First player wins!");
             Console.WriteLine("Score: {0}", totalScoreFirstPlayer);
             Console.WriteLine("Games won: {0}", winsFirstPlayer);
         }
-        else if (winsFirstPlayer < winsSecondPlaer)
+        else if (totalScoreFirstPlayer < totalScoreSecondPlayer)
         {
             Console.WriteLine("Second player wins!");
             Console.WriteLine("Score: {0}", totalScoreSecondPlayer);
@@ -162,87 +157,5 @@ class CardWars
             Console.WriteLine("It's a tie!");
             Console.WriteLine("Score: {0}", totalScoreSecondPlayer);
         }
-    }
-
-    private static string RemoveZeroFromString(string cardsPlayer, int indexOfZero)
-    {
-        for (int i = 0; i < cardsPlayer.Length; i++)
-        {
-            indexOfZero = cardsPlayer.IndexOf('0');
-
-            if (indexOfZero > 0)
-            {
-                cardsPlayer = cardsPlayer.Remove(indexOfZero, 1);
-                i = indexOfZero - 1;
-                indexOfZero = 0;
-            }
-        }
-        return cardsPlayer;
-    }
-
-    private static BigInteger CheckForSpecialCards(string cardPlayer, int j, BigInteger totalScore)
-    {
-        if (cardPlayer[j] == 'Y')
-        {
-            totalScore -= 200;
-        }
-        else if (cardPlayer[j] == 'Z')
-        {
-            totalScore *= 2;
-
-        }
-
-        return totalScore;
-    }
-
-    private static int GetScoreForGame(int scorePlayer, string cardsPlayer)
-    {
-
-        switch (cardsPlayer)
-        {
-            case "2":
-                scorePlayer += 10;
-                break;
-            case "3":
-                scorePlayer += 9;
-                break;
-            case "4":
-                scorePlayer += 8;
-                break;
-            case "5":
-                scorePlayer += 7;
-                break;
-            case "6":
-                scorePlayer += 6;
-                break;
-            case "7":
-                scorePlayer += 5;
-                break;
-            case "8":
-                scorePlayer += 4;
-                break;
-            case "9":
-                scorePlayer += 3;
-                break;
-            case "1":
-                scorePlayer += 2;
-                break;
-            case "A":
-                scorePlayer += 1;
-                break;
-            case "J":
-                scorePlayer += 11;
-                break;
-            case "Q":
-                scorePlayer += 12;
-                break;
-            case "K":
-                scorePlayer += 13;
-                break;
-            default:
-                throw new ArgumentException();
-                break;
-        }
-        return scorePlayer;
     }
 }
---
+++ Programming with C#/1. C# Fundamentals I/0. BGCoder C#Part1/07. 24 June 2013 Evening/04. Bat’Goiko Tower/BatGoikoTower.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class BatGoikoTower
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int hight = n;
+        int width = 2 * n;
+        int[,] matrix = new int[hight, width];
+
+        int currentRow = n - 1;
+        int currentCol = 0;
+
+        while (currentRow >= 0)
+        {
+            matrix[currentRow, currentCol] = 1;
+            currentRow--;
+            currentCol++;
+
+        }
+
+        currentRow++;
+
+        while (currentRow <= n - 1)
+        {
+            matrix[currentRow, currentCol] = 2;
+            currentRow++;
+            currentCol++;
+        }
+
+        int countCrossbeams = 2;
+        for (int row = 1; row < n; row++)
+        {
+            for (int col = n - row; col < 2 * n - (n - row); col++)
+            {
+                matrix[row, col] = 3;
+            }
+
+            if ((row += countCrossbeams) < n)
+            {
+                row--;
+                countCrossbeams++;
+            }
+        }
+
+        // print
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                if (matrix[row, col] == 0)
+                {
+                    Console.Write(".");
+                }
+                else if (matrix[row, col] == 1) //  && col < n)
+                {
+                    Console.Write("/");
+                }
+                else if (matrix[row, col] == 2) // && col >= n && col < 2 * n)
+                {
+                    Console.Write("\\");
+                }
+                else if (matrix[row, col] == 3)
+                {
+                    Console.Write("-");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
