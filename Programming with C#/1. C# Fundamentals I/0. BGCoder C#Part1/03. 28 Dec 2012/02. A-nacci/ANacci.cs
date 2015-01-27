be0cede26910b3d605ac364b6a384a57b6072d9e
Todor Dakov <tdd@dekom.bg>
2015-01-27 02:42:23 +0200

    Update

---
+++ Programming with C#/1. C# Fundamentals I/0. BGCoder C#Part1/03. 28 Dec 2012/02. A-nacci/ANacci.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.A_nacci
+{
+    class ANacci
+    {
+        static void Main()
+        {
+            // A = 65
+            // Z = 90
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+
+            string alfaBet = "0ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int arrLenght = (2 * l);
+            int[] array = new int[arrLenght];
+
+            array[0] = first - 64;
+            array[1] = second - 64;
+
+            if (l == 1)
+            {
+                Console.WriteLine(alfaBet[array[0]]);
+            }
+            else
+            {
+                for (int i = 2; i < array.Length - 1; i++)
+                {
+                    int sum = 0;
+                    sum = array[i - 1] + array[i - 2];
+                    if (sum < 27)
+                    {
+                        array[i] = sum;
+                    }
+                    else
+                    {
+                        array[i] = sum % 26;
+                    }
+                }
+
+                int count = 0;
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (i == 0)
+                    {
+                        Console.WriteLine(alfaBet[array[i]]);
+                    }
+                    else if (i == 1 || i == 2)
+                    {
+                        Console.Write(alfaBet[array[i]]);
+                        Console.Write("{0}\n", alfaBet[array[i + 1]]);
+                        i++;
+                    }
+                    else if (i > 2)
+                    {
+                        Console.Write(alfaBet[array[i]]);
+                        if (count <= l - 2)
+                        {
+                            count++;
+                        }
+                        int spase = 1;
+                        while (spase <= count)
+                        {
+                            Console.Write(" ");
+                            spase++;
+                        }
+
+                        Console.Write("{0}\n", alfaBet[array[i + 1]]);
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Programming with C#/1. C# Fundamentals I/0. BGCoder C#Part1/12. 2011-2Part1SampleEx/02. Miss Cat 2011 Vik/MissCat2011Vik.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Miss_Cat_2011_Vik
+{
+    class MissCat2011Vik
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[11];
+
+            for (int i = 0; i < n; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+                array[numbers]++;
+            }
+                        
+            int maxNumber = int.MinValue;
+            int index = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (maxNumber < array[i])
+                {
+                    maxNumber = array[i];
+                    index = i;
+                }
+            }
+          
+            Console.WriteLine(index);
+        }
+    }
+}
