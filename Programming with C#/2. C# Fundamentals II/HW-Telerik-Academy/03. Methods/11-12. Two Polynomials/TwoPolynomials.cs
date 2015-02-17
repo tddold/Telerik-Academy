e98c2ecee9be8841a28160b28e4584d1bc9297b8
Todor Dakov <tdd@dekom.bg>
2015-02-18 01:18:17 +0200

    Update

--- Programming with C#/2. C# Fundamentals II/HW-Telerik-Academy/03. Methods/11-12. Two Polynomials/TwoPolynomials.cs
+++ Programming with C#/2. C# Fundamentals II/HW-Telerik-Academy/03. Methods/11-12. Two Polynomials/TwoPolynomials.cs
@@ -23,26 +23,164 @@ class TwoPolynomials
         decimal[] firstPolynom = EnterPolynom(out firstPolynom);
         PrintSeparateLine();
 
-        Console.WriteLine("Print first polinom:");
+        Console.WriteLine("Print first polinom:\n");
         PrintPolinom(firstPolynom);
+        PrintSeparateLine();
 
         decimal[] secondPolynom = EnterPolynom(out secondPolynom);
         PrintSeparateLine();
 
-        Console.WriteLine("Print second polinom:");
+        Console.WriteLine("Print second polinom:\n");
         PrintPolinom(secondPolynom);
         PrintSeparateLine();
 
+        Console.WriteLine(" Adding polynomials:\n");
+        AddingPolynomials(firstPolynom, secondPolynom);
+        PrintSeparateLine();
 
+        Console.WriteLine(" Subtraction polynomials:\n");
+        SubtractionPolynomials(firstPolynom, secondPolynom);
+        PrintSeparateLine();
+
+        Console.WriteLine(" Multiplication polynomials:\n");
+        MultiplicationPolynomials(firstPolynom, secondPolynom);
+        PrintSeparateLine();
     }
 
-    private static void PrintPolinom(decimal[] polynom)
+    private static decimal[] EnterPolynom(out decimal[] polynom)
+    {
+        Console.Write("Enter the exponent of the polinom :");
+        byte expo = byte.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        polynom = new decimal[expo + 1];
+
+        for (int i = polynom.Length - 1; i >= 0; i--)
+        {
+            Console.Write(" x^" + i + ": ");
+            polynom[i] = decimal.Parse(Console.ReadLine());
+        }
+
+        return polynom;
+    }
+
+    static void AddingPolynomials(decimal[] firstPolynom, decimal[] secondPolynom)
+    {
+        PrintPolinom(firstPolynom);
+        Console.Write("   +\n");
+        PrintPolinom(secondPolynom);
+        Console.Write("   =\n");
+
+        int biggerLenght = Math.Max(firstPolynom.Length, secondPolynom.Length);
+        bool isFirstBigger = firstPolynom.Length >= secondPolynom.Length ? true : false;
+        bool isSecondBigger = !isFirstBigger;
+        bool isZero = true;
+
+        var result = new decimal[biggerLenght];
+
+        for (int i = biggerLenght - 1; i >= 0; i--)
+        {
+            if (i < firstPolynom.Length && i < secondPolynom.Length)
+            {
+                result[i] = firstPolynom[i] + secondPolynom[i];
+                if (result[i] != 0)
+                {
+                    isZero = false;
+                }
+            }
+            else if (isFirstBigger)
+            {
+                result[i] = firstPolynom[i];
+            }
+            else if (isSecondBigger)
+            {
+                result[i] = secondPolynom[i];
+            }
+        }
+
+        if (!isZero)
+        {
+            PrintPolinom(result);
+        }
+        else
+        {
+            Console.WriteLine("   0\n");
+        }
+    }
+
+    static void SubtractionPolynomials(decimal[] firstPolynom, decimal[] secondPolynom)
+    {
+        PrintPolinom(firstPolynom);
+        Console.Write("   -\n");
+        PrintPolinom(secondPolynom);
+        Console.Write("   =\n");
+
+        int biggerLenght = Math.Max(firstPolynom.Length, secondPolynom.Length);
+        bool isFirstBigger = firstPolynom.Length >= secondPolynom.Length ? true : false;
+        bool isSecondBigger = !isFirstBigger;
+        bool isZero = true;
+
+        var result = new decimal[biggerLenght];
+
+        for (int i = biggerLenght - 1; i >= 0; i--)
+        {
+            if (i < firstPolynom.Length && i < secondPolynom.Length)
+            {
+                result[i] = firstPolynom[i] - secondPolynom[i];
+                if (result[i] != 0)
+                {
+                    isZero = false;
+                }
+            }
+            else if (isFirstBigger)
+            {
+                result[i] = firstPolynom[i];
+            }
+            else if (isSecondBigger)
+            {
+                result[i] = -secondPolynom[i];
+            }
+        }
+
+        if (!isZero)
+        {
+            PrintPolinom(result);
+        }
+        else
+        {
+            Console.WriteLine("   0\n");
+        }
+    }
+
+    private static void MultiplicationPolynomials(decimal[] firstPolynom, decimal[] secondPolynom)
+    {
+        PrintPolinom(firstPolynom);
+        Console.Write("   *\n");
+        PrintPolinom(secondPolynom);
+        Console.Write("   =\n");
+
+
+
+        var result = new decimal[firstPolynom.Length + secondPolynom.Length];
+
+        for (int i = 0; i < firstPolynom.Length; i++)
+        {
+            for (int j = 0; j < secondPolynom.Length; j++)
+            {
+                result[i + j] += firstPolynom[i] * secondPolynom[j];
+            }
+        }
+
+        PrintPolinom(result);
+    }
+
+    static void PrintPolinom(decimal[] polynom)
     {
         for (int i = polynom.Length - 1; i >= 0; i--)
         {
-            if (i == polynom.Length - 1 && polynom[i] !=0)
+            if (i == polynom.Length - 1 && polynom[i] != 0)
             {
-                Console.Write("    {0}{1}x^{2} ", polynom[i] > 0 ? "" : " -", Math.Abs(polynom[i]), i);
+                Console.Write("    {0}{1}x^{2} ", polynom[i] > 0 ? "" : "-", Math.Abs(polynom[i]), i);
             }
             else if (i == 0)
             {
@@ -50,7 +188,7 @@ class TwoPolynomials
                 {
                     Console.Write("{0}{1} ", "- ", -polynom[i]);
                 }
-                else if(polynom[i] > 0)
+                else if (polynom[i] > 0)
                 {
                     Console.Write("{0}{1} ", "+ ", polynom[i]);
                 }
@@ -63,7 +201,7 @@ class TwoPolynomials
                 }
                 else if (polynom[i] > 0)
                 {
-                    Console.Write("{0}{1}x^{2} ", polynom[polynom.Length - 1] == 0 ? "" : "+ ", polynom[i], i);
+                    Console.Write("{0}{1}x^{2} ", (polynom[polynom.Length - 1] == 0 ? "" : "+ "), polynom[i], i);
                 }
             }
         }
@@ -71,24 +209,9 @@ class TwoPolynomials
         Console.WriteLine();
     }
 
-    private static decimal[] EnterPolynom(out decimal[] polynom)
-    {
-        Console.Write("Enter the exponent of the polinom :");
-        byte expo = byte.Parse(Console.ReadLine());
 
-        polynom = new decimal[expo + 1];
 
-        for (int i = polynom.Length - 1; i >= 0; i--)
-        {
-            Console.Write("x^" + i + ": ");
-            polynom[i] = decimal.Parse(Console.ReadLine());
-        }
-
-        return polynom;
- 
-    }
-
-    public static void PrintSeparateLine()
+    static void PrintSeparateLine()
     {
         Console.WriteLine(new string('-', 40));
     }
