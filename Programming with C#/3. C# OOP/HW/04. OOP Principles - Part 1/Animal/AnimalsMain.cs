3df72f008833d67eb46defbf858696dd13fb9108
Todor Dakov <tdd@dekom.bg>
2015-03-28 18:06:18 +0200

    Update

---
+++ Programming with C#/3. C# OOP/HW/04. OOP Principles - Part 1/Animal/AnimalsMain.cs
@@ -0,0 +1,14 @@
+﻿namespace Animal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class AnimalsMain
+    {
+        public static void Main()
+        {
+        }
+    }
+}
