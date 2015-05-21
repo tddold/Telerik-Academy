90a8204ff9f85791f4be3d3dd42e275984746525
Todor Dakov <tdd@dekom.bg>
2015-05-22 00:37:41 +0300

    Update

--- Web Design &UI/3. JS-Fundamentals/HW/03. Conditional statements/Conditional statements/scripts/script.js
+++ Web Design &UI/3. JS-Fundamentals/HW/03. Conditional statements/Conditional statements/scripts/script.js
@@ -15,7 +15,7 @@ a	b	result
 
 
 function task1() {
-
+    jsConsole.clearConsole();
     document.getElementsByTagName( "h2" )[0].innerHTML = "Problem 1. Exchange if greater";
     document.getElementsByTagName( "p" )[1].innerHTML = "Problem 1. Exchange if greater";
     document.getElementsByTagName( "p" )[0].innerHTML = "Write an if statement that takes two double variables a and b and exchanges their values if the first one is greater than the second.";
@@ -25,14 +25,12 @@ function task1() {
         greater,
         smaller;
 
-        var input = document.createElement("input");
-		input.type = "text";
-		input.className = "css-class-name"; // set the CSS class
-		container.appendChild(input); // put it into the DOM
+    //var input = document.createElement("input");
+    //input.type = "text";
+    //input.className = "css-class-name"; // set the CSS class
+    //container.appendChild(input); // put it into the DOM
 
-		 document.body.insertBefore(createElement, container); 
-
-    console.log( a );
+    // //document.body.insertBefore(createElement, container);    
 
     if ( a > b ) {
         greater = a;
@@ -46,7 +44,8 @@ function task1() {
     jsConsole.writeLine();
     jsConsole.writeLine( "---------------------------------------------------------------------------" );
 
-    jsConsole.writeLine( 'Result is: ' + smaller + ' ' + greater);
+    jsConsole.writeLine( 'Result is: ' + smaller + ' ' + greater );
+    console.log( 'Result is: ' + smaller + ' ' + greater );
 
     jsConsole.writeLine( "---------------------------------------------------------------------------" );
 
@@ -58,39 +57,90 @@ function task1() {
 //Use a sequence of if operators.
 //Examples:
 
-//a	b	c	result
-//5	2	2	+
-//-2	-2	1	+
-//-2	4	3	-
-//0	-2.5	4	0
-//-1	-0.5	-5.1	-
+// a	 b	    c	    result
+// 5     2	    2	    +
+//-2	-2	    1	    +
+//-2	 4	    3	    -
+// 0  -2.5	    4	    0
+//-1  -0.5	 -5.1	    -
 
 function task2() {
+    jsConsole.clearConsole();
 
     document.getElementsByTagName( "h2" )[0].innerHTML = "Problem 2. Multiplication Sign";
     document.getElementsByTagName( "p" )[1].innerHTML = "Problem 2. Multiplication Sign";
     document.getElementsByTagName( "p" )[0].innerHTML = "Write a script that shows the sign (+, - or 0) of the product of three real numbers, without calculating it. Use a sequence of if operators.";
 
+    var a = jsConsole.readFloat( "#first" ),
+        b = jsConsole.readFloat( '#second' ),
+        c = jsConsole.readFloat( '#third' ),
+        minus = false,
+        plus = true,
+        zero = false;
+
+    if ( a === 0 || b === 0 || c === 0 ) {
+        zero = true;
+        plus = false;
+
+    } else if ( ( a < 0 && b < 0 && c < 0 ) ||
+         ( ( a < 0 && ( b > 0 && c > 0 ) || ( b < 0 && ( a > 0 && c > 0 ) ) || ( c < 0 && ( b > 0 && a > 0 ) ) ) ) ) {
+        minus = true;
+        plus = false;
+    }
+
+
     jsConsole.writeLine();
     jsConsole.writeLine( "---------------------------------------------------------------------------" );
 
-   
+
+    if ( plus ) {
+        jsConsole.writeLine( 'Result is: "+"' );
+
+    } else if ( minus ) {
+        jsConsole.writeLine( 'Result is: "-"' );
+
+    } else if ( zero ) {
+        jsConsole.writeLine( 'Result is: 0' );
+
+    }
 
     jsConsole.writeLine( "---------------------------------------------------------------------------" );
 
 }
 
+//Problem 3. The biggest of Three
 
+//Write a script that finds the biggest of three numbers.
+//Use nested if statements.
+//    Examples:
+
+//  a	    b	    c	    biggest
+//  5	    2	    2	    5
+// -2	    -2	    1	    1
+// -2	    4	    3	    4
+//  0	    -2.5	5	    5
+// -0.1	    -0.5	-1.1	-0.1
 
 function task3() {
+    jsConsole.clearConsole();
 
-    
-   
+    document.getElementsByTagName( "h2" )[0].innerHTML = "Problem 3. The biggest of Three";
+    document.getElementsByTagName( "p" )[1].innerHTML = "Problem 3. The biggest of Three";
+    document.getElementsByTagName( "p" )[0].innerHTML = "Write a script that finds the biggest of three numbers. Use nested if statements.";
+
+    var a = jsConsole.readFloat( "#first" ),
+        b = jsConsole.readFloat( '#second' ),
+        c = jsConsole.readFloat( '#third' ),
+        minus = false,
+        plus = true,
+        zero = false;
+
+
 
     jsConsole.writeLine();
     jsConsole.writeLine( "---------------------------------------------------------------------------" );
 
-    
+
 
     jsConsole.writeLine( "---------------------------------------------------------------------------" );
 
