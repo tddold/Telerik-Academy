9636956b99d770d8c0bbe9dffb1ab5ebbd39a3cd
Todor Dakov <tdd@dekom.bg>
2015-07-11 03:34:48 +0300

    Update

---
+++ Programming with C#/4. High-Quality-Code/HW/05. VariablDataExpAndConst/Figure rotation/Figure.cs
@@ -0,0 +1,82 @@
+﻿namespace Figure_rotation
+{
+    using System;
+
+    public class Figure
+    {
+        private double width;
+        private double height;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Figure"/> class.
+        /// </summary>
+        /// <param name="width">The width of the figure.</param>
+        /// <param name="height">The height of the figure.</param>
+        public Figure(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        /// <summary>
+        /// Gets or sets the width of the figure.
+        /// </summary>
+        public double Width
+        {
+            get
+            {
+                return this.Width;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width must be greater than zero.");
+                }
+
+                this.Width = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the height of the figure.
+        /// </summary>
+        public double Height
+        {
+            get
+            {
+                return this.Height;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height must be greater than zero.");
+                }
+
+                this.Height = value;
+            }
+        }
+
+        /// <summary>
+        /// Rotate figure by given angle of rotation.
+        /// </summary>
+        /// <param name="figure">The figure to rotate.</param>
+        /// <param name="rotationAngel">The angle of rotation.</param>
+        /// <returns>A new figure after rotation by the given angle of rotation.</returns>
+        public static Figure Ratate(Figure figure, double rotationAngel)
+        {
+            double cosValue = Math.Abs(Math.Cos(rotationAngel));
+            double sinValue = Math.Abs(Math.Sin(rotationAngel));
+
+            double newWidth = (cosValue * figure.Width) + (sinValue * figure.Height);
+            double newHeight = (sinValue * figure.Width) + (cosValue * figure.Height);
+
+            var rotatedFigure = new Figure(newWidth, newHeight);
+
+            return rotatedFigure;
+        }
+    }
+}
---
+++ Programming with C#/4. High-Quality-Code/HW/05. VariablDataExpAndConst/Print Statistic/Printer.cs
@@ -0,0 +1,52 @@
+﻿namespace Print_Statistic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Printer
+    {
+        public void PrintStatistic(double[] numbers, int count)
+        {
+            double largestNumber = double.MinValue;
+            double smallestNumber = double.MaxValue;
+            double sum = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (numbers[i] > largestNumber)
+                {
+                    largestNumber = numbers[i];
+                }
+
+                if (numbers[i] < smallestNumber)
+                {
+                    smallestNumber = numbers[i];
+                }
+
+                sum += numbers[i];
+            }
+
+            double average = sum / count;
+
+            this.PrintMax(largestNumber);
+            this.PrintMin(smallestNumber);
+            this.PrintAverage(average);
+        }
+
+        private void PrintMin(double smallestNumber)
+        {
+            Console.WriteLine("The smallest number is {0}.", smallestNumber);
+        }
+
+        private void PrintMax(double largestNumber)
+        {
+            Console.WriteLine("The largest number is {0}.", largestNumber);
+        }
+
+        private void PrintAverage(double average)
+        {
+            Console.WriteLine("The average of the numbers is {0}.", average);
+        }
+    }
+}
