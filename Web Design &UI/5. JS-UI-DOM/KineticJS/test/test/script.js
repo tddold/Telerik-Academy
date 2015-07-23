3ef3210eae5f63d327d231d1f964df240da86b66
tddold <tdd@dekom.bg>
2015-07-23 04:20:25 +0300

    KineticJS

---
+++ Web Design &UI/5. JS-UI-DOM/KineticJS/test/test/script.js
@@ -0,0 +1,23 @@
+﻿var layer = new Kinetic.Layer(),
+    ball = new Kinetic.Circle({
+        x: 0,
+        y: 0,
+        fill: 'pink',
+        stroke: 'purple'
+    });
+console.log(ball);
+
+layer.add(ball);
+stage.add(layer);
+
+function animFrame() {
+    var x = ball.getX() + 5,
+        y = ball.getY() + 10;
+
+    ball.setX(x);
+    ball.setY(y);
+
+    layer.draw;
+
+    setTimeout(animFrame, 300);
+}
