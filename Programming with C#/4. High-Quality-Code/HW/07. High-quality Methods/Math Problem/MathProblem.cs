cc047a372ec79e5981a927f6a3b408ca645d6771
Todor Dakov <tdd@dekom.bg>
2015-07-24 18:29:01 +0300

    HW7

--- Programming with C#/4. High-Quality-Code/HW/07. High-quality Methods/Math Problem/MathProblem.cs
+++ Programming with C#/4. High-Quality-Code/HW/07. High-quality Methods/Math Problem/MathProblem.cs
@@ -1,26 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Math_Problem
+﻿namespace Math_Problem
 {
-    class MathProblem
-    {
-        static ulong baseSystem = 19;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
 
-        static void Main()
+    public class MathProblem
+    {
+        private static ulong baseSystem = 19;
+
+        public static void Main()
         {
             // arr 19 base system
-            char[] arr = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's' };
+            char[] arrayOfChars = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's' };
 
             // read input
             string[] input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
-            // calc
-            StringBuilder number19th = new StringBuilder();
-            ulong number10th = 0;
+            // logic
+            StringBuilder numberInNineteenNumericSystem = new StringBuilder();
+            ulong number = ConvertInputToNumber(arrayOfChars, input);
+            ulong result = CalculateResult(arrayOfChars, numberInNineteenNumericSystem, number);
+
+            // output
+            Console.WriteLine("{0} = {1}", numberInNineteenNumericSystem, result);
+        }
+
+        /// <summary>
+        /// Method that calculates decimal number system in nineteen number system.
+        /// </summary>
+        /// <param name="arrayOfChars">Input symbols.</param>
+        /// <param name="numberInNineteenNumericSystem">String in nineteen number system.</param>
+        /// <param name="number">Converts number.</param>
+        /// <returns>Integer number.</returns>
+        private static ulong CalculateResult(char[] arrayOfChars, StringBuilder numberInNineteenNumericSystem,  ulong number)
+        {
+            ulong result = number;
+
+            if (number == 0)
+            {
+                result = 'a';
+            }
+            else
+            {
+                while (number > 0)
+                {
+                    int digits = (int)(number % baseSystem);
+                    numberInNineteenNumericSystem.Insert(0, arrayOfChars[digits]);
+                    number /= baseSystem;
+                }
+            }
+
+            return result;
+        }        
+
+        /// <summary>
+        /// Method that calculates input to number.
+        /// </summary>
+        /// <param name="arrayOfChars">All chars.</param>
+        /// <param name="input">Input.</param>
+        /// <returns>Number.</returns>
+        private static ulong ConvertInputToNumber(char[] arrayOfChars, string[] input)
+        {
+            ulong number = 0;
             ulong subResult = 0;
 
             foreach (var word in input)
@@ -28,33 +70,15 @@ namespace Math_Problem
                 for (int i = 0; i < word.Length; i++)
                 {
                     int pow = word.Length - 1 - i;
-                    int currNumber = Array.IndexOf(arr, word[i]);
+                    int currNumber = Array.IndexOf(arrayOfChars, word[i]);
                     subResult += (ulong)currNumber * Power(pow);
-
                 }
 
-                number10th += subResult;
+                number += subResult;
                 subResult = 0;
             }
 
-            ulong result = number10th;
-
-            if (number10th == 0)
-            {
-                result = 'a';
-            }
-            else
-            {
-                while (number10th > 0)
-                {
-                    int digits = (int)(number10th % baseSystem);
-                    number19th.Insert(0, arr[digits]);
-                    number10th /= baseSystem;
-                }
-            }
-
-            // output
-            Console.WriteLine("{0} = {1}", number19th, result);
+            return number;
         }
 
         private static ulong Power(int pow)
---
+++ Programming with C#/4. High-Quality-Code/HW/07. High-quality Methods/Sum of Differences/SumOfDifference.cs
@@ -0,0 +1,104 @@
+﻿namespace Sum_of_Differences
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SumOfDifference
+    {
+        /// <summary>
+        /// The  method that calculates the sum of the differences.
+        /// </summary>
+        public static void Main()
+        {
+            // input
+            long[] input = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => long.Parse(x))
+                .ToArray();
+
+            // logic
+            long result = 0;
+
+            result = CalculateSumOfAllOddNumbers(input, result);
+
+            // output
+            Console.WriteLine(result);
+        }
+
+        /// <summary>
+        /// The method that calculates the sum of all odd numbers.
+        /// </summary>
+        /// <param name="input">Input.</param>
+        /// <param name="result">Result.</param>
+        /// <returns>Integer number.</returns>
+        private static long CalculateSumOfAllOddNumbers(long[] input, long result)
+        {
+            for (int i = 1; i < input.Length; i++)
+            {
+                long numberA = input[i];
+                long numberB = input[i - 1];
+                long bigger = 0;
+                long smaller = 0;
+
+                CheckNumberIsBigger(numberA, numberB, ref bigger, ref smaller);
+
+                long diff = bigger - smaller;
+
+                if (diff % 2 == 0)
+                {
+                    if (i + 2 < input.Length)
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    if (i + 1 < input.Length)
+                    {
+                        result += diff;
+                        diff = 0;
+                    }
+                    else
+                    {
+                        if (diff != 0)
+                        {
+                            result += diff;
+                        }
+
+                        break;
+                    }
+                }
+
+                diff = 0;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// The method that calculates the greater of two numbers.
+        /// </summary>
+        /// <param name="numberA">First number.</param>
+        /// <param name="numberB">Second number.</param>
+        /// <param name="bigger">A greater number.</param>
+        /// <param name="smaller">Тhe lower number.</param>
+        private static void CheckNumberIsBigger(long numberA, long numberB, ref long bigger, ref long smaller)
+        {
+            if (numberA > numberB)
+            {
+                bigger = numberA;
+                smaller = numberB;
+            }
+            else
+            {
+                bigger = numberB;
+                smaller = numberA;
+            }
+        }
+    }
+}
