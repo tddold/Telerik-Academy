86fb0f995f8e62f497d3372efe7902aaa54c42e2
Todor Dakov <tdd@dekom.bg>
2015-07-28 20:00:37 +0300

    HW14

---
+++ Web Design &UI/5. JS-UI-DOM/HW2014/DOM Operations/Task2/Task2/scripts/script.js
@@ -0,0 +1,59 @@
+﻿/*Write a script that creates 5 div elements and moves them in circular path with interval of 100 milliseconds
+*/
+
+var frag = document.createDocumentFragment(),
+    divElement = document.createElement('div'),
+    currentDiv,
+    i,
+    angel,
+    radius,
+    COUNT = 5;
+
+divElement.style.display = 'ínline-block';
+divElement.style.width = '50px';
+divElement.style.height = '50px';
+divElement.style.border = '2px solid yellowgreen';
+divElement.style.marginRight = '25px';
+divElement.style.position = 'relative';
+divElement.style.top = '200px';
+divElement.style.left = '100px';
+
+for (i = 0; i < COUNT; i += 1) {
+    currentDiv = divElement.cloneNode(true);
+
+    frag.appendChild(currentDiv);
+}
+
+document.body.appendChild(frag);
+
+animate();
+
+angel = 3 * Math.PI / 180;
+radius = 40;
+
+function animate() {
+    var divs,
+        i,
+        len,
+        newX,
+        newY,
+        current;
+
+    divs = document.getElementsByTagName('div');
+    len = divs.length;
+
+    angel += 20 * Math.PI / 180;
+
+    for (i = 0; i < len; i += 1) {
+        current = divs[i];
+
+       
+        newX = parseInt(current.style.top, 10) + radius * Math.sin(angel);
+        newY = parseInt(current.style.left, 10) + radius * Math.cos(angel);
+
+        current.style.top = newX + 'px';
+        current.style.left = newY + 'px';
+    }
+
+    setTimeout(animate, 100);
+}
---
+++ Web Design &UI/5. JS-UI-DOM/HW2014/DOM Operations/Task2/Task4/scripts/script.js
@@ -0,0 +1,43 @@
+﻿/*Create a tag cloud:
+Visualize a string of tags (strings) in a given container
+By given minFontSize and maxFontSize, generate the tags with different font-size, depending on the number of occurrences
+*/
+
+
+var tags,
+    tagCloud;
+
+tags = ["cms", "javascript", "js", "ASP.NET MVC", ".net", ".net", "css", "wordpress", "xaml", "js", "http", "web", "asp.net", "asp.net MVC", "ASP.NET MVC", "wp", "javascript", "js", "cms", "html", "javascript", "http", "http", "CMS"]
+
+tagCloud = generateTagCloud(tags, 17, 42);
+
+function generateTagCloud(tags, miniFontSize, maxFontSize) {
+    var dictionary,
+        frag;
+
+    dictionary = makeDictionary(tags);
+    frag = document.createDocumentFragment();
+
+    document.body.appendChild(frag);
+}
+
+function makeDictionary(collections) {
+    var i,
+        len,
+        currentTag,
+        dictionary = {};
+
+    len = collections.length;
+
+    for (i = 0; i < len; i += 1) {
+        currentTag = collections[i];
+
+        if (!dictionary[currentTag]) {
+            dictionary[currentTag] = 0;
+        }
+
+        dictionary[currentTag] += 1;
+    }
+
+    return dictionary;
+}
