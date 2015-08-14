3d1f36ab7235383918fba59e29b944faf3ffefdf
Todor Dakov <tdd@dekom.bg>
2015-08-14 14:46:42 +0300

    Implement class Cell.cs

---
+++ Programming with C#/4. High-Quality-Code/HW/13. Refactoring/Homework/Cell.cs
@@ -0,0 +1,23 @@
+﻿namespace GameFifteen
+{
+    using System;
+    public class Cell
+    {
+        public Cell()
+        {
+        }
+
+        public Cell(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Value = 1;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Value { get; set; }
+    }
+}
--- Programming with C#/4. High-Quality-Code/HW/13. Refactoring/Homework/Matrix.cs
+++ Programming with C#/4. High-Quality-Code/HW/13. Refactoring/Homework/Matrix.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace RotatingWalkInMatrix
+﻿namespace RotatingWalkInMatrix
 {
+    using System;
     class Matrix
     {
     }
