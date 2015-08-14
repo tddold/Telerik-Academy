9b11125e0136e996327df7605180ed5a86dfab4a
Todor Dakov <tdd@dekom.bg>
2015-08-14 17:26:35 +0300

    Create first and second test

---
+++ Programming with C#/4. High-Quality-Code/HW/13. Refactoring/RotatingWallkInMatrixTest/RotatingWallkInMatrixTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace RotatingWallkInMatrix.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RotatingWalkInMatrix;
+
+    [TestClass]
+    public class RotatingWallkInMatrixTests
+    {
+        [TestMethod]
+        [Timeout(100)]
+        public void CreateMatrixByZeroSizeTest()
+        {
+            byte n = 0;
+            var expectedMatrix = new int[n,n];
+            var actualMatrix = Matrix.GenerrateMatrix(n);
+
+            var areMatrixesEquale = this.AreMatrixEqual(expectedMatrix, actualMatrix);
+            Assert.IsTrue(areMatrixesEquale);
+        }
+
+        [TestMethod]
+        [Timeout(100)]
+        public void CreateMatrixBySizeEqualOneTest()
+        {
+            byte n = 1;
+            var expectedMatrix = new int[,]
+            {
+                {1 }
+            };
+
+            var actualMatrix = Matrix.GenerrateMatrix(n);
+
+            var areMatrixesEqual = this.AreMatrixEqual(expectedMatrix, actualMatrix);
+            Assert.IsTrue(areMatrixesEqual);
+
+        }
+
+        private bool AreMatrixEqual(int[,] expectedMatrix, int[,] actualMatrix)
+        {
+            for (int i = 0; i < expectedMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < expectedMatrix.GetLength(1); j++)
+                {
+                    if (expectedMatrix[i,j] != actualMatrix[i,j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
