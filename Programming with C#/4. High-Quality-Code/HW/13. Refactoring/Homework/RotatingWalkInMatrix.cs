8608a15c9474800246789df14e00cc19c3f50832
Todor Dakov <tdd@dekom.bg>
2015-08-14 15:06:37 +0300

    Refacturing If and For statemant

--- Programming with C#/4. High-Quality-Code/HW/13. Refactoring/Homework/RotatingWalkInMatrix.cs
+++ Programming with C#/4. High-Quality-Code/HW/13. Refactoring/Homework/RotatingWalkInMatrix.cs
@@ -6,13 +6,22 @@
         static void change(ref int dx, ref int dy)
         {
             int[] dirX = { 1, 1, 1, 0, -1, -1, -1, 0 };
-
-
             int[] dirY = { 1, 0, -1, -1, -1, 0, 1, 1 };
+
             int cd = 0;
+
             for (int count = 0; count < 8; count++)
-                if (dirX[count] == dx && dirY[count] == dy) { cd = count; break; }
-            if (cd == 7) { dx = dirX[0]; dy = dirY[0]; return; }
+            {
+                if (dirX[count] == dx && dirY[count] == dy)
+                {
+                    cd = count; break;
+                }
+            }
+
+            if (cd == 7)
+            {
+                dx = dirX[0]; dy = dirY[0]; return;
+            }
             dx = dirX[cd + 1];
 
 
@@ -22,15 +31,28 @@
         {
             int[] dirX = { 1, 1, 1, 0, -1, -1, -1, 0 };
             int[] dirY = { 1, 0, -1, -1, -1, 0, 1, 1 };
+
             for (int i = 0; i < 8; i++)
             {
-                if (x + dirX[i] >= arr.GetLength(0) || x + dirX[i] < 0) dirX[i] = 0;
+                if (x + dirX[i] >= arr.GetLength(0) || x + dirX[i] < 0)
+                {
+                    dirX[i] = 0;
+                }
 
+                if (y + dirY[i] >= arr.GetLength(0) || y + dirY[i] < 0)
+                {
+                    dirY[i] = 0;
+                }
 
-                if (y + dirY[i] >= arr.GetLength(0) || y + dirY[i] < 0) dirY[i] = 0;
             }
+
             for (int i = 0; i < 8; i++)
-                if (arr[x + dirX[i], y + dirY[i]] == 0) return true;
+            {
+                if (arr[x + dirX[i], y + dirY[i]] == 0)
+                {
+                    return true;
+                }
+            }
 
             return false;
         }
@@ -39,10 +61,18 @@
         {
             x = 0;
             y = 0;
-            for (int i = 0; i < arr.GetLength(0); i++)
 
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
                 for (int j = 0; j < arr.GetLength(0); j++)
-                    if (arr[i, j] == 0) { x = i; y = j; return; }
+                {
+                    if (arr[i, j] == 0)
+                    {
+                        x = i; y = j;
+                        return;
+                    }
+                }
+            }
 
         }
 
@@ -63,11 +93,19 @@
             { //malko e kofti tova uslovie, no break-a raboti 100% : )
                 matrix[i, j] = k;
 
-                if (!proverka(matrix, i, j)) { break; } // prekusvame ako sme se zadunili
+                if (!proverka(matrix, i, j))
+                {
+                    break;
+                } // prekusvame ako sme se zadunili
+
                 if (i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrix[i + dx, j + dy] != 0)
+                {
+                    while ((i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrix[i + dx, j + dy] != 0))
+                    {
+                        change(ref dx, ref dy);
+                    }
+                }
 
-
-                    while ((i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrix[i + dx, j + dy] != 0)) { change(ref dx, ref dy); }
                 i += dx; j += dy; k++;
             }
             //for (int p = 0; p < n; p++)
@@ -88,16 +126,24 @@
                 while (true)
                 { //malko e kofti tova uslovie, no break-a raboti 100% : )
                     matrix[i, j] = k;
-                    if (!proverka(matrix, i, j)) { break; }// prekusvame ako sme se zadunili
+                    if (!proverka(matrix, i, j))
+                    {
+                        break;
+                    }// prekusvame ako sme se zadunili
+
                     if (i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrix[i + dx, j + dy] != 0)
+                    {
 
-
-                        while ((i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrix[i + dx, j + dy] != 0)) change(ref dx, ref dy);
+                        while ((i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrix[i + dx, j + dy] != 0))
+                        {
+                            change(ref dx, ref dy);
+                        }
+                    }
                     i += dx; j += dy; k++;
                 }
 
             }
-           
+
         }
 
         public static void PrintMatrix(int[,] matrix)
