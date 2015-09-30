3c9b3bb8fdceba10cd13c3b7f242faed61ba28a5
Todor Dakov <tdd@dekom.bg>
2015-09-30 16:47:32 +0300

    Update

---
+++ Databases/02. Processing XML in .NET/Processing XML in .NET/08. Create Album XML/CreateAlbumXML.cs
@@ -0,0 +1,59 @@
+﻿namespace _08.Create_Album_XML
+{
+    using System;
+    using System.Text;
+    using System.Xml;
+
+    public class CreateAlbumXML
+    {
+        private const string File = "../../catalogue.xml";
+        private const string NewFile = "../../album.xml";
+
+        public static void Main()
+        {
+            GenerateAlbumXML();
+        }
+
+        private static void GenerateAlbumXML()
+        {
+            XmlTextWriter writer = new XmlTextWriter(NewFile, Encoding.GetEncoding("utf-8"));
+            using (writer)
+            {
+                writer.WriteStartDocument();
+                writer.Formatting = Formatting.Indented;
+                writer.IndentChar = '\t';
+                writer.Indentation = 1;
+                writer.WriteStartElement("albums");
+
+                string name = string.Empty;
+
+                using (XmlReader reader = XmlReader.Create(File))
+                {
+                    while (reader.Read())
+                    {
+                        if (reader.NodeType == XmlNodeType.Element && reader.Name == "name")
+                        {
+                            name = reader.ReadElementContentAsString();
+                        }
+                        else if (reader.NodeType == XmlNodeType.Element && reader.Name == "artist")
+                        {
+                            string artist = reader.ReadElementContentAsString();
+                            WriteAlbum(writer, name, artist);
+                        }
+                    }
+                }
+
+                writer.WriteEndDocument();
+                Console.WriteLine("Document {0} was created.", NewFile);
+            }
+        }
+
+        private static void WriteAlbum(XmlTextWriter writer, string name, string artist)
+        {
+            writer.WriteStartElement("album");
+            writer.WriteElementString("name", name);
+            writer.WriteElementString("artist", artist);
+            writer.WriteEndElement();
+        }
+    }
+}
---
+++ Databases/02. Processing XML in .NET/Processing XML in .NET/09.Traverse Directories/TraverseDirectories.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace _09.Traverse_Directories
+{
+    internal class TraverseDirectories
+    {
+        private const string RootDirectory = "../../";
+        private const string File = "../../file.xml";
+
+        private static void Main()
+        {
+            CreateXmlDirectoriesTree();
+            Console.WriteLine("Document {0} was created.", File);
+        }
+
+        private static void CreateXmlDirectoriesTree()
+        {
+            var rootDirectory = new DirectoryInfo(RootDirectory);
+
+            using (var writer = new XmlTextWriter(File, Encoding.GetEncoding("utf-8")))
+            {
+                writer.Formatting = Formatting.Indented;
+                writer.IndentChar = '\t';
+                writer.Indentation = 2;
+
+                writer.WriteStartDocument();
+                writer.WriteStartElement("directories");
+                TraverseRootDirectory(writer, rootDirectory);
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+
+            }
+        }
+
+        private static void TraverseRootDirectory(XmlTextWriter writer, DirectoryInfo rootDirectory)
+        {
+            if (!rootDirectory.GetFiles().Any() && !rootDirectory.GetDirectories().Any())
+            {
+                return;
+            }
+
+            writer.WriteStartElement("dir");
+            writer.WriteStartAttribute("name", rootDirectory.Name);
+
+            foreach (var file in rootDirectory.GetFiles())
+            {
+                writer.WriteStartElement("file");
+                writer.WriteStartAttribute("name", file.Name);
+                writer.WriteEndElement();
+            }
+
+            foreach (var dir in rootDirectory.GetDirectories())
+            {
+                TraverseRootDirectory(writer, dir);
+            }
+
+            writer.WriteEndElement();
+        }
+    }
+}
