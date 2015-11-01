8410065d73d4ee307d02894e15ed09b2d3b80a1f
tddold <tdd@dekom.bg>
2015-11-01 14:49:24 +0200

    Added the ASP.NET Web API project and moved users and DbContext to separate DLLs

---
+++ Web-Services-and-Cloud/Tests/SourceControlSystem/Data/SourceControlSystem.Data/SourseControlSystemDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace SourceControlSystem.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+
+    public class SourseControlSystemDbContext : IdentityDbContext<User>
+    {
+        public SourseControlSystemDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static SourseControlSystemDbContext Create()
+        {
+            return new SourseControlSystemDbContext();
+        }
+    }
+}
---
+++ Web-Services-and-Cloud/Tests/SourceControlSystem/Data/SourceControlSystem.Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace SourceControlSystem.Models
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager, string authenticationType)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
