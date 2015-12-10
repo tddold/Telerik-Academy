6c6efc227261b09af6e9354db471d934bb2e0f63
tddold <tdd@dekom.bg>
2015-12-11 01:39:50 +0200

    Update

---
+++ Web Design &UI/7. SPA/music-app/app/js/controllers/EditArtistController.js
@@ -0,0 +1,19 @@
+﻿'use strict'
+
+musicApp.controller('EditArtistController',
+    function EditArtistController($scope) {
+
+        $scope.saveArtist = function (artist, newArtisForm) {
+            if (newArtisForm.$valid) {
+            alert('Artist saved:' + artist.name + ' ' + artist.created); //TODO save artist
+            } else {
+                alert('Invalid data');
+            }
+
+        }
+
+        $scope.cancel = function () {
+            alert('cancel'); //TODO cancel, redirect
+        }
+    }
+);
