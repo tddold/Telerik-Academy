6765da970928ec49c39150b1a56de4e25a431311
tddold <tdd@dekom.bg>
2015-12-30 03:15:27 +0200

    Update

---
+++ Web Design and UI/8. End-to-End/Test/MogoDbAndMongoos/MogoDbAndMongoos/app.js
@@ -0,0 +1,61 @@
+﻿//var mongodb = require('mongodb');
+
+//var server = new mongodb.Server('localhost', 27017);
+//var mongoClient = new mongodb.MongoClient(server);
+
+//mongoClient.open(function (Error, mongoClient) {
+//    var db = mongoClient.db('student');
+//    var courses = db.collection('courses');
+
+//    courses.insert({ name: 'NodeJS', lectures: 10 },
+//         function (err, courses) {
+//             if (err) {
+//            console.log(err);
+//             }
+
+//        console.log(courses);
+//    });
+//});
+
+/*
+* This example uses the node MongoDB module to connect to the local
+* mongodb database on this virtual machine
+*
+* More here: http://mongodb.github.io/node-mongodb-native/markdown-docs/collections.html
+*/
+
+//require node modules (see package.json)
+var mongodb = require('mongodb'),
+    MongoClient = mongodb.MongoClient,
+    format = require('util').format,
+    url = 'mongodb://127.0.0.1:27017/test';
+
+
+//connect away
+MongoClient.connect(url, function (err, db) {
+    if (err) {
+        throw err;
+    }
+    console.log("Connected to Database");
+    
+    //create collection
+    db.createCollection("testCollection", function (err, collection) {
+        if (err) {
+            throw err;
+        }
+        
+        console.log("Created testCollection");
+
+        collection.insert({
+            name: 'AngularJS',
+            lectures:10
+        })
+
+        collection
+        .find({ $or: [{ name: 'AngularJS' }, { name: 'NodeJS' }] })
+        .toArray(function (err, result) {
+            console.log(result);
+        })
+    });
+});
+
---
+++ Web Design and UI/8. End-to-End/Test/MogoDbAndMongoos/MogoDbAndMongoos/server.js
@@ -0,0 +1,53 @@
+﻿var mongoose = require('mongoose'),
+    url = 'localhost:27017/students',
+    Stusent,
+    student,
+    studentSchema,
+    Course,
+    courseSchema,
+    db;
+
+mongoose.connect(url);
+
+db = mongoose.connection;
+
+db.once('open', function (err) {
+    if (err) {
+        console.log(err);
+    }
+    
+    console.log('MongoDB database up and running...')
+});
+
+db.on('error', function (err) {
+    console.log(err);
+});
+
+studentSchema = new mongoose.Schema({
+    firstName: { type: String , require: true },
+    lastName: String,
+    age: { type: Number, min: 0, max: 120, require: true }
+});
+
+Student = mongoose.model('Student', studentSchema);
+
+courseSchema = new mongoose.Schema({
+    name: String,
+    students: [studentSchema]
+});
+
+Course = mongoose.model('Course', courseSchema); String
+Student.find({}).exec(function (err, result) {
+    var peshoStudent = result[3];
+    var nodeJsCourse = new Course();
+    nodeJsCourse.name = 'End-to-End JS Apps';
+    nodeJsCourse.students.push(peshoStudent);
+    nodeJsCourse.save(function (err, result) {
+        console.log(result); 
+    });
+
+    Course.find({ name: 'End-toEnd JS Apps' })
+    .exec(function (err, result) {
+        console.log(result);
+    });
+});
