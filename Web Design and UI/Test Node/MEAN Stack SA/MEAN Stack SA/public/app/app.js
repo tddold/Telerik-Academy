dddc08791b57bb1350f0b0323d3385117528cefe
tddold <tdd@dekom.bg>
2016-01-03 19:28:38 +0200

    Add angular app and home-controller

---
+++ Web Design and UI/Test Node/MEAN Stack SA/MEAN Stack SA/public/app/app.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+    
+    function config($routeProvider, $locationProvider) {
+        var CONTROLLER_VEW_MODEL_NAME = 'vm';
+        
+        $locationProvider.html5Mode(true);
+        
+        $routeProvider
+            .when('/', {
+            templateUrl: '/partials/home',
+            controller: 'HomeController',
+            controllerAs: CONTROLLER_VEW_MODEL_NAME
+        })
+            .otherwise({ redirectTo: '/' });
+    };
+    
+    angular.module('myApp.services', []);
+    angular.module('myApp.derivtives', []);
+    angular.module('myApp.controllers', ['myApp.services']);
+    
+    angular.module('myApp', ['ngResource', 'ngRoute', 'ngCookies',
+        'myApp.controllers', 'myApp.derivtives'])
+        .config(['$routeProvider', '$locationProvider', config])
+        .constant('baseUrl', 'http://localhost:3030')
+}());
---
+++ Web Design and UI/Test Node/MEAN Stack SA/MEAN Stack SA/public/app/home/home-controller.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+    
+    function HomeController() {
+        var vm = this;
+        debugger;
+        vm.hello = 'Hello Angular!';
+    };
+    
+
+
+    angular.module('myApp.controllers')
+    .controller('HomeController', [HomeController])
+}());
