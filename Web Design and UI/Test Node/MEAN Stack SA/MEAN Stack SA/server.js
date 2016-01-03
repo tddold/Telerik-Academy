3f036e33e04d7740244f545a54e1e6b64254a0b3
tddold <tdd@dekom.bg>
2016-01-03 22:55:19 +0200

    Add angular features

--- Web Design and UI/Test Node/MEAN Stack SA/MEAN Stack SA/server.js
+++ Web Design and UI/Test Node/MEAN Stack SA/MEAN Stack SA/server.js
@@ -5,12 +5,7 @@
     env = process.env.NOD_ENV || 'development',
     port = process.env.PORT || 3030,
     app = express(),
-    messageSchema,
-    messageFromDataBase,
-    Message,
     db;
-
-
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/server/views');
@@ -26,10 +21,8 @@ app.use(express.static(__dirname + '/public'));
 
 // mongodb
 if (env === 'development') {
-    console.log('development');
     mongoose.connect('mongodb://localhost/means')
 } else {
-    console.log('production');
     mongoose.connect('mongodb://admin:means1234@ds037015.mongolab.com:37015/means');
 }
 
@@ -48,41 +41,13 @@ db.on('error', function (err) {
     console.log('Database error: ' + err);
 });
 
-messageSchema = mongoose.Schema({
-    message: String
-})
-
-Message = mongoose.model('Message', messageSchema);
-
-Message.remove({})
-    .exec(function (err) {
-    if (err) {
-        console.log('Message could not be cleared: ' + err);
-        return;
-    }
-    
-    console.log('Messages deleted!');
-    
-    Message.create({ message: 'Hi for Mongoose!' })
-        .then(function (model) {
-        if (err) {
-            console.log('Cannot be created database: ' + err);
-            return;
-        }
-        
-        messageFromDataBase = model.message;
-        console.log(model.message);
-    });
-});
-
 app.get('/partials/:partialName', function (req, res) {
     res.render('partials/' + req.params.partialName);
 });
 
 app.get('*', function (req, res) {
-    res.render('index', { message: messageFromDataBase });
+    res.render('index');
 });
 
 app.listen(port);
 console.log('Server running on port: ' + port);
-console.log(env);
