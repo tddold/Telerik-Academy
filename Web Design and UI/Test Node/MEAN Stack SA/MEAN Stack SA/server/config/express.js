808d6ab15b1cd2bb646a6a91ec79baf02114fd7e
tddold <tdd@dekom.bg>
2016-01-04 02:24:27 +0200

    Implemented ligin to the server

--- Web Design and UI/Test Node/MEAN Stack SA/MEAN Stack SA/server/config/express.js
+++ Web Design and UI/Test Node/MEAN Stack SA/MEAN Stack SA/server/config/express.js
@@ -1,11 +1,16 @@
 ﻿var express = require('express'),
     stylus = require('stylus'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    passport = require('passport');
 
 module.exports = function (app, config) {
     app.set('view engine', 'jade');
     app.set('views', config.rootPath + '/server/views');
+    app.use(cookieParser());
     app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(session());
     app.use(stylus.middleware({
         src: config.rootPath + '/public',
         compile: function (str, path) {
@@ -13,5 +18,7 @@ module.exports = function (app, config) {
         }
     }));
     
+    app.use(passport.initialize());
+    app.use(passport.session());
     app.use(express.static(config.rootPath + '/public'));
 };
--- Web Design and UI/Test Node/MEAN Stack SA/MEAN Stack SA/server/config/mongoose.js
+++ Web Design and UI/Test Node/MEAN Stack SA/MEAN Stack SA/server/config/mongoose.js
@@ -1,4 +1,8 @@
 ﻿var mongoose = require('mongoose'),
+    passport = require('passport'),
+    localPassport = require('passport-local'),
+    userSchema,
+    User,
     db;
 
 module.exports = function (config) {
@@ -17,5 +21,65 @@ module.exports = function (config) {
     
     db.on('error', function (err) {
         console.log('Database error: ' + err);
+    });
+    
+    userSchema = mongoose.Schema({
+        username: String,
+        firstName: String,
+        lastName: String
+        // salt: String,
+        // hashPass: String
+    });
+    
+    User = mongoose.model('User', userSchema);
+    
+    User.find({}).exec(function (err, collection) {
+        if (err) {
+            console.log('Cannot find users' + err);
+            return;
+        }
+        
+        if (collection.length === 0) {
+            User.create({ username: 'Pesho', firstName: 'Pesho', lastName: 'Peshev' });
+            User.create({ username: 'Ivan', firstName: 'Ivan', lastName: 'Ivanov' });
+            User.create({ username: 'Gosho', firstName: 'Gosho', lastName: 'Goshev' });
+            console.log('Users added to database...');
+        }
+    });
+    
+    passport.use(new localPassport(function (username, password, done) {
+        User.findOne({ username: username }).exec(function (err, user) {
+            if (err) {
+                console.log('Error loading user: ' + err);
+                return;
+            }
+            
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        })
+    }));
+    
+    passport.serializeUser(function (user, done) {
+        if (user) {
+            return done(null, user._id);
+        }
+    });
+    
+    passport.deserializeUser(function (id, done) {
+        User.findOne({ _id: id }).exec(function (err, user) {
+            if (err) {
+                console.log('Error loading user: ' + err);
+                return;
+            }
+            
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        });
     });
 };
